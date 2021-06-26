722b12c58620aa1781af9f7f367227aace2e393f
steve <steve@MARGARET-2021>
2021-06-26 12:59:17 -0700

    Add project files.

---
+++ IvieBingo2021/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace IvieBingo2021
+{
+    partial class frmBingo
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.lblCurrentCall = new System.Windows.Forms.Label();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.pbxCurrentPicture = new System.Windows.Forms.PictureBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCurrentPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Location = new System.Drawing.Point(5, 76);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(608, 1192);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Previous:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.Font = new System.Drawing.Font("Segoe UI", 20.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 89;
+            this.listBox1.Items.AddRange(new object[] {
+            "Steve",
+            "Margaret",
+            "Kevin",
+            "Deborah Harper"});
+            this.listBox1.Location = new System.Drawing.Point(8, 47);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.listBox1.Size = new System.Drawing.Size(585, 1068);
+            this.listBox1.TabIndex = 0;
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(12, 12);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(288, 58);
+            this.btnNew.TabIndex = 2;
+            this.btnNew.Text = "New &Game";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // lblCurrentCall
+            // 
+            this.lblCurrentCall.AutoSize = true;
+            this.lblCurrentCall.Location = new System.Drawing.Point(638, 12);
+            this.lblCurrentCall.Name = "lblCurrentCall";
+            this.lblCurrentCall.Size = new System.Drawing.Size(97, 41);
+            this.lblCurrentCall.TabIndex = 3;
+            this.lblCurrentCall.Text = "label1";
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(311, 12);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(287, 58);
+            this.btnNext.TabIndex = 4;
+            this.btnNext.Text = "&Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(40, 40);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(2506, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // pbxCurrentPicture
+            // 
+            this.pbxCurrentPicture.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbxCurrentPicture.Location = new System.Drawing.Point(651, 99);
+            this.pbxCurrentPicture.Name = "pbxCurrentPicture";
+            this.pbxCurrentPicture.Size = new System.Drawing.Size(1676, 1081);
+            this.pbxCurrentPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbxCurrentPicture.TabIndex = 6;
+            this.pbxCurrentPicture.TabStop = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // frmBingo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2506, 1275);
+            this.Controls.Add(this.pbxCurrentPicture);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.lblCurrentCall);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmBingo";
+            this.Text = "IVIE Bingo 2021";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCurrentPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Label lblCurrentCall;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.PictureBox pbxCurrentPicture;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ IvieBingo2021/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IvieBingo2021
+{
+    public partial class frmBingo : Form
+    {
+        private static readonly string ITEMS_PATH = "C:\\Steve\\BINGO\\Names2021.txt";
+        private static List<String> items;
+        private static List<String> currentGameList;
+        private static int currentIndex = 0;
+        private static readonly Random rnd = new Random();
+
+        public frmBingo()
+        {
+            InitializeComponent();
+            
+            string[] itemStrings = File.ReadAllLines(ITEMS_PATH);
+            items = new List<string>(itemStrings);
+            currentGameList = new List<string>(items);
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            lblCurrentCall.Text = currentGameList.ElementAt(currentIndex++);
+            listBox1.Items.Insert(0,lblCurrentCall.Text);
+            if (currentIndex >= currentGameList.Count)
+            {
+                btnNext.Enabled = false;
+            }
+            
+        }
+
+        private void Shuffle(IList<String> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                int k = (rnd.Next(0, n));
+                n--;
+                String value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Are you sure you want to clear and start a new game?", "Start New Game?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dr.Equals(DialogResult.Yes)){
+                Shuffle(currentGameList);
+                listBox1.Items.Clear();
+                btnNext.Enabled = true;
+                lblCurrentCall.Text = "";
+                currentIndex = 0;
+            }
+        }
+    }
+}
